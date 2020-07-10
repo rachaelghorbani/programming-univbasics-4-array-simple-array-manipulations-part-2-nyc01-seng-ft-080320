bdd83cb2597cb9fd8a128b2e0e88a0fd735b3a0f
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 21:00:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end 
 
-def using_insert(array1, new_element)
-  
+def using_insert(array, new_element)
+  array.insert(4, new_element)
 end 
